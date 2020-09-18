e214d0cd056577b663a2dcf4237d6d73c5bec005
vagava <2380@holbertonschool.com>
2020-09-18 00:26:43 +0000

    add5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+* main - print a text string with printf
+* Return: cero
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
