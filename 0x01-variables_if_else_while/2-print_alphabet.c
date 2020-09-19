82fe682f52618f3b15accf4fdfab7a3b22bf3f48
vagava <2380@holbertonschool.com>
2020-09-19 04:12:45 +0000

    add2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercase
+ * Return: cero
+*/
+int main(void)
+{
+	char i;
+	for (i='a'; i <= 'z' ; i++)
+	{
+		putchar(i);
+		putchar('\n');
+	}
+	return (0);
+}
