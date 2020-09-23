e5641d40801e21e6859bb792944911d4b51cd8b0
vagava <2380@holbertonschool.com>
2020-09-23 05:07:37 +0000

    add9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * times_table - print a trable 9x9
+ * Return: void
+ *
+ */
+void times_table(void)
+{
+	int i = 0, j, r;
+
+	while (i <= 9)
+	{
+		j = 0;
+		while (j <= 9)
+		{
+			r= i*j;
+			_putchar(r / 10 + '0');
+			_putchar(r % 10 + '0');
+			_putchar(32);
+			j++;
+		}
+		_putchar(10);
+		i++;
+	}
+}
