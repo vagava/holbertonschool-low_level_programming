19934b45b9b694efecb64f9a6a6861795eed0efb
vagava <2380@holbertonschool.com>
2020-09-23 01:27:45 +0000

    upgrade5

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,6 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
+
+#endif
