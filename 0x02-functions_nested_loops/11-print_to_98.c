e04fd1460bac558c532e90344e54dfd0e4567479
vagava <2380@holbertonschool.com>
2020-09-24 06:24:02 +0000

    modified-11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,22 +11,18 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i < 98; i++)
 		{
 		printf("%d, ", i);
-		if (i == 98)
-		{
-			printf("%d", i);
-		}
 		}
 	}
 	else
 	{
-		for (i = n; i >= 98; i--)
+		for (i = n; i > 98; i--)
 		{
 			printf("%d, ", i);
 		}
 	}
-	printf("\n");
+	printf("98\n");
 }
 
