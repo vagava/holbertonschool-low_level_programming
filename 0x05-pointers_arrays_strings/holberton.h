8e27ffc010a3f474c85dddf329fb692bae484f53
vagava <2380@holbertonschool.com>
2020-09-29 16:50:53 +0000

    add putchar holberton.h

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,11 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+void reset_to_98(int *n);
+void _puts(char *str);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+int _putchar(char c);
+void print_rev(char *s);
+void rev_string(char *s);
+
+#endif
