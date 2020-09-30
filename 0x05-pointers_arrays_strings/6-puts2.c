fa46a752e49eaf8e3f84412d6b516947eed649b4
vagava <2380@holbertonschool.com>
2020-09-30 04:11:54 +0000

    add6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,17 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * puts2 - print even positions
+ * @str: string to print
  */
 void puts2(char *str)
 {
 	int i = 0;
-	for (i = 0; str [i] != '\0'; i++)
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i % 2 == 0)
 		{
-		_putchar(str [i]);
+		_putchar(str[i]);
 		}
 	}
 	_putchar('\n');
