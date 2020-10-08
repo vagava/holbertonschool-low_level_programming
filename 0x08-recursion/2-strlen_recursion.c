7d2fc56118b47ad7aa53bd82b70600343ee3bd06
vagava <2380@holbertonschool.com>
2020-10-08 15:13:08 -0500

    modified2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,10 +2,12 @@
 /**
  * _strlen_recursion - contar caracteres
  * @s: string
+ * Return: number of chars
  */
 int _strlen_recursion(char *s)
 {
 	int n = 0;
+
 	if (*s != '\0')
 	{
 		n = _strlen_recursion(s + 1);
