e494f3b4a6992428f113b9ba2ba5743ca91a1237
vagava <2380@holbertonschool.com>
2020-10-08 15:02:15 -0500

    add2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - contar caracteres
+ * @s: string
+ */
+int _strlen_recursion(char *s)
+{
+  int n = 0;
+    if (*s != '\0')
+    {
+       n = _strlen_recursion(s + 1);
+       n = n + 1;
+    }
+   return (n);
+}
