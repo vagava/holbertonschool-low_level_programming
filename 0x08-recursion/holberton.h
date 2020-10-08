22712f95543f5b9186fb0afd15ba156936637c9a
vagava <2380@holbertonschool.com>
2020-10-07 23:47:08 -0400

    add-all

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -7,5 +7,11 @@ int _strlen(char *s);
 int _putchar(char c);
 void print_rev(char *s);
 void rev_string(char *s);
+int _strlen_recursion(char *s);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 
 #endif
