167b4075462c1193d57e960e1b5f2ed540e102bd
vagava <2380@holbertonschool.com>
2020-10-08 15:06:25 -0500

    modified2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,11 +5,11 @@
  */
 int _strlen_recursion(char *s)
 {
-  int n = 0;
-    if (*s != '\0')
-    {
-       n = _strlen_recursion(s + 1);
-       n = n + 1;
-    }
-   return (n);
+	int n = 0;
+	if (*s != '\0')
+	{
+		n = _strlen_recursion(s + 1);
+		n = n + 1;
+	}
+	return (n);
 }
