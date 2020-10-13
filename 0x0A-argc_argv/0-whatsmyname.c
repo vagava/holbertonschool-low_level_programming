58d483874287aad82f64f6682a9e4805062747ae
vagava <2380@holbertonschool.com>
2020-10-12 22:49:06 -0500

    fix-betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,12 @@
 #include "holberton.h"
 /**
  * main - first argument
+ * @argc: unused
+ * @argv: string
+ * Return: string
  */
-int main(int argc __attribute__((unused)), char ** argv)
+int main(int argc __attribute__((unused)), char **argv)
 {
- printf("%s\n", argv[0]);
-  return 0;
+	printf("%s\n", argv[0]);
+	return (0);
 }
