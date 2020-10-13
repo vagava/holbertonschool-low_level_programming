ee9b568d04c5014e1f9d06418f0bdab2b137ada8
vagava <2380@holbertonschool.com>
2020-10-12 22:56:33 -0500

    add1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * main - first argument
+ * @argc: unused
+ * @argv: string
+ * Return: string
+ */
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
