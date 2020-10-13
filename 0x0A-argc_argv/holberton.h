276b91b812238340f4e2b2b5d8c884faaea50300
vagava <2380@holbertonschool.com>
2020-10-12 22:40:47 -0500

    add.h

---
+++ 0x0A-argc_argv/holberton.h
@@ -0,0 +1,19 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+#include<stdio.h>
+void reset_to_98(int *n);
+void _puts(char *str);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+int _putchar(char c);
+void print_rev(char *s);
+void rev_string(char *s);
+int _strlen_recursion(char *s);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+int main (int argc, char ** argv);
+
+#endif
