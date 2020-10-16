1ee99d1aaf78001ccb6e9a47d813f229399420ad
vagava <vagava.123@gmail.com>
2020-10-16 15:54:56 -0500

    fix_betty

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,25 +13,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int c1, c2, a, res = 0, cp;
 
 	if (s1 == NULL)
-	{	s1 = "";
-	}
+		s1 = "";
 	if (s2 == NULL)
-	{	s2 = "";
-	}
+		s2 = "";
 	for (c1 = 0; s1[c1] != '\0'; c1++)
 	{}
 	for (c2 = 0; s2[c2] != '\0'; c2++)
 	{}
 	if (n >= c2)
-	{ n = c2;
-	}
+		n = c2;
 	res = c1 + n + 1;
-
 	new = malloc(res * sizeof(char));
 	if (new == NULL)
-	{
 		return (NULL);
-	}
 	for (cp = 0; cp < c1; cp++)
 	{
 		*(new + cp) = *(s1 + cp);
