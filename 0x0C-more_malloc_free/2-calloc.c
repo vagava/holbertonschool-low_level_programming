b0b38badda2282080b1a1dab682cd9e6ae71da7a
vagava <vagava.123@gmail.com>
2020-10-17 10:03:36 -0500

    fixed code and betty

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,13 +1,19 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
- *
- *
+ * _calloc - allocates memory
+ * @nmemb: elemnts
+ * @size: size of data type in bytes
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *v = malloc(nmemb * size);
 	unsigned int i;
+
+	if (v == NULL)
+		return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
 	for (i = 0; i < (nmemb * size); i++)
 	{
