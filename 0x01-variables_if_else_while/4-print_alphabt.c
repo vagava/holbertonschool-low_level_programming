80859e089a521be824a133051b7a4747d6d79c1d
vagava <vagava.123@gmail.com>
2020-10-17 19:56:24 -0500

    fix_code

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,8 +8,11 @@ int main(void)
 {
 	int i;
 
-	for (i = 97; (i != 101 || i != 113) && i <= 122; i++)
+	for (i = 97; i <= 122; i++)
+	{
+		if (i != 101 && i != 113)
 		putchar(i);
+	}
 	putchar(10);
 	return (0);
 }
