6106a11e29c3347cab0a9e4f71891f90a5cb0dc0
vagava <vagava.123@gmail.com>
2020-10-17 20:03:10 -0500

    add_5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,14 @@
-prueba
+#include <stdio.h>
+/**
+ * main - print numbers for 0 to 9
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	putchar(10);
+	return (0);
+}
