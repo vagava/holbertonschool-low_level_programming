ac85c08885efddd6c9b397ea2e6937ad39f8bcf5
vagava <vagava.123@gmail.com>
2020-10-21 17:01:22 -0500

    add-0, readme, file.h

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - print a name
+ * @name: name to print
+ * @f: pointer to function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if(name && f)
+		f(name);
+}
