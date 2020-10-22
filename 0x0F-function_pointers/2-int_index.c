d5eb497c4caac542f8c98cf7c964514b29cf293d
vagava <vagava.123@gmail.com>
2020-10-22 12:09:55 -0500

    add_2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,11 +10,18 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array && size > 0 && cmp)
+	if (array == NULL || size <= 0 || cmp == NULL)
+	{
+		return (-1);
+	}
+	else
+	{
 		for (i = 0; i < size; i++)
 		{
 			cmp(array[i]);
+			if ((cmp(array[i])) != 0)
+			return (i);
 		}
-	else
-		return (-1);
+	}
+	return (-1);
 }
