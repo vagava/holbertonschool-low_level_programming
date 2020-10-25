8ee100beb6561f6b2a133049aa725b1f6d88a75f
vagava <vagava.123@gmail.com>
2020-10-25 18:58:30 -0500

    fix_code_1

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,14 +7,18 @@
  */
 int main(int argc, char **argv)
 {
+	int a1, a2;
+
+	a1 = atoi(argv[1]);
+	a2 = atoi(argv[3]);
+	
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 	if ((get_op_func(argv[2])) != NULL )
-		printf("%d\n", get_op_func(argv[2])		\
-		       (atoi (argv[1]), atoi (argv[3])));
+		printf("%d\n", get_op_func(argv[2])(a1, a2));
 	else
 	{
 		printf("Error\n");
