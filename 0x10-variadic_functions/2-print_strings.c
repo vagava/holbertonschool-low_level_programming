b2bf003aba007735e852f5fac8f40696e0c10e7b
vagava <vagava.123@gmail.com>
2020-10-25 11:53:26 -0500

    add_2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings - imprime cadenas de caracteres
+ * @separator: separa las cadenas
+ * @n: numero de argumentos
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	/*variable tipo va_list*/
+	va_list list;
+	/*inicializa la macro*/
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		/*recorre la lista de argumentos*/
+		char *str = va_arg(list, char *);
+
+		if (str != NULL)
+			printf("%s", str);
+		else
+			printf("nill");
+		if (separator != NULL && i != n - 1)
+			printf("%c", *separator);
+	}
+	/*libera la memoria*/
+	va_end(list);
+	printf("\n");
+}
