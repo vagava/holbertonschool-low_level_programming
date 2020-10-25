a12a299fe28444c4cc05c3165e2c6a217992192c
vagava <vagava.123@gmail.com>
2020-10-24 21:47:32 -0500

    fix_code_1

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,15 +10,17 @@ int sum_them_all(const unsigned int n, ...)
 {
 	if (n == 0)
 		return (0);
-
+/* definir variable tipo va_list*/
 	va_list list;
 	int result = 0;
-
+/*inicializar macro con lista de argumentos y argumentos requeridos*/
 	va_start(list, n);
+/*ciclo para recorrer los argumentos dados*/
 	for (unsigned int i = 0; i < n; i++)
 	{
-		result = result + va_arg(list, int);
+		result = result + va_arg(list, unsigned int);
 	}
+/*liberar la memoria asignada*/
 	va_end(list);
 	return (result);
 }
