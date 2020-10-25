95fb550312e2b18fc5c7cbfb6ee367dd30cc4078
vagava <vagava.123@gmail.com>
2020-10-25 18:50:36 -0500

    fix_code_0

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,5 @@
 #include "3-calc.h"
+#include <stddef.h>
 /**
  * get_op_func - recorre y compara la estructura op
  * @s: igualador de operador.
@@ -6,12 +7,21 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
 	int i = 0;
 
 	while (ops[i].op != NULL)
 	{
-		if (*s = *ops[i].op)
-			return (ops[i].f);
-		i++;
+		if (*s != *ops[i].op)
+			i++;
 	}
+	return (ops[i].f);
 }
