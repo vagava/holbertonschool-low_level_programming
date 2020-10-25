b286c97b2408b7c48e07a910cf67f52d80d74714
vagava <vagava.123@gmail.com>
2020-10-25 18:51:25 -0500

    fix_code_0

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,18 +1,28 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * main - recibe argumentos para realizar operaciones
  *
  */
-int main(int argc __attribute__((unused)), char **argv)
+int main(int argc, char **argv)
 {
-	int (*p_fun_op);
-	int res, a1, a2;
-
-	a1 = atoi(argv[1]);
-	a2 = atoi(argv[3]);
-
-	p_fun_op = get_op_func(argv[2]);
-
-	res = (p_fun_op)(a1, a2);
-	printf("%d", res);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((get_op_func(argv[2])) != NULL )
+		printf("%d\n", get_op_func(argv[2])		\
+		       (atoi (argv[1]), atoi (argv[3])));
+	else
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
+	   {
+		printf("Error\n");
+		exit(100);
+	}
 }
