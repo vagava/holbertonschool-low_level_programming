d5a8840d4facc63803a9edd928aec451f9b03ee5
vagava <vagava.123@gmail.com>
2020-10-25 19:11:26 -0500

    fix_code_2

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,7 +3,9 @@
 #include <stdlib.h>
 /**
  * main - recibe argumentos para realizar operaciones
- *
+ * @argc: cantidad de argumentos
+ * @argv: argumentos
+ * return: cero
  */
 int main(int argc, char **argv)
 {
@@ -11,13 +13,13 @@ int main(int argc, char **argv)
 
 	a1 = atoi(argv[1]);
 	a2 = atoi(argv[3]);
-	
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if ((get_op_func(argv[2])) != NULL )
+	if ((get_op_func(argv[2])) != NULL)
 		printf("%d\n", get_op_func(argv[2])(a1, a2));
 	else
 	{
@@ -25,8 +27,9 @@ int main(int argc, char **argv)
 		exit(99);
 	}
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
-	   {
-		printf("Error\n");
-		exit(100);
+	{
+		   printf("Error\n");
+		   exit(100);
 	}
+	return (0);
 }
