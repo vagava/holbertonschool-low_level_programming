f2f9b4979f6c2af1ab98ddec0eee63e65c589eec
vagava <vagava.123@gmail.com>
2020-10-25 21:18:12 -0500

    fix_code_7

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,7 +16,11 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
-	
+	if ((get_op_func(argv[2])) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
 	{
 		printf("Error\n");
