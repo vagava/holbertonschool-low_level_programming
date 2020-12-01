f54f4dcb4df013f92c1ea8d2c872371175527a45
vagava <vagava.123@gmail.com>
2020-12-01 09:43:38 -0500

    fix_betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,13 @@
 #include <unistd.h>
+/**
+ * main - main is void
+ * Return: 1
+ */
 
-int main (void)
+int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
-	return(1);
+	write(2, "and that piece of art is useful\" - Dora Korpar,\
+	2015-10-19\n", 60);
+	return (1);
 }
 
