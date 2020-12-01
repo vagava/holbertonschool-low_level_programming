8314dcfedf4d5ea82925b40ec6155bbbc6d2543b
vagava <vagava.123@gmail.com>
2020-12-01 11:01:24 -0500

    add_102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of two two-digit numbers.
+ * Return: 0.
+ */
+int main(void)
+{
+	int i, j;
+
+	for (j = 0; j <= 99; j++)
+	{
+		for (i = j + 1; i <= 99; i++)
+		{
+			if (i != j)
+			{
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				putchar(' ');
+				if (j < 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
