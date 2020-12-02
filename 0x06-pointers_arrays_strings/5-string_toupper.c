9f0659b406cb5bc48ab64ee6926e9b13d56b734e
vagava <vagava.123@gmail.com>
2020-12-02 18:34:13 -0500

    add_5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ * @s: string
+ * Return: string modified
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	for (i = 0; s[i] != 0, i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		s[i] = s[i] - 32;
+	}
+	return (s);
+}
