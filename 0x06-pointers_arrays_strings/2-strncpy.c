f4b943908e71b76da9c144ed2648354907a906fe
vagava <vagava.123@gmail.com>
2020-12-02 17:56:07 -0500

    add_2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _strncpy - copies a string.
+ * @dest: string destino.
+ * @src: string para agregar.
+ * @n:size.
+ * Return: pointer to dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int contador = 0, size = 0;
+
+	for (; dest[size] != '\0'; size++)
+	{}
+	for (contador = 0; ((contador < n) && (src[contador] != '\0')); contador++)
+	{
+		dest[size + contador] = src[contador];
+	}
+	return (dest);
+}
