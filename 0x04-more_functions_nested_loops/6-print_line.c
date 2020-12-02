0d592c7ba6bd5ab434bfea3262823595a137384e
vagava <vagava.123@gmail.com>
2020-12-01 23:18:39 -0500

    add_6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_line - draws a straight line in the terminal.
+ * @n:  is the number of times the character _ should be printed
+ */
+void print_line(int n)
+{
+	int i;
+	for (i = 0; i <= n; i++)
+	{
+		if(n != 0)
+		_putchar('_');
+	}
+	_putchar('\n');
+}
