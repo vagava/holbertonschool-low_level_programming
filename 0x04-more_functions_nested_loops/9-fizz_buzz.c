f756428945db411e42708ea449d3bddf95d670b9
vagava <vagava.123@gmail.com>
2020-12-02 00:20:44 -0500

    fix_code spaces

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,27 +8,32 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for(i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if(i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else
 		{
-			if (i < 100)
-				printf("%d ", i);
-			else
+			if (i == 1)
+			{
 				printf("%d", i);
+			}
+			else
+			{
+				printf(" %d", i);
+			}
 		}
 	}
+	printf("\n");
 	return (0);
 }
