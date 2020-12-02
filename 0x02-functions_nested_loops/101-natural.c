2ef13f3f2efbd96bf0e40e3a7678ee0163f594f5
vagava <vagava.123@gmail.com>
2020-12-01 19:07:15 -0500

    add_101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - computes and prints the sum of all the multiples of 3 or 5.
+ * Return: 0
+ */
+int main (void)
+{
+	int i, add = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if(((i % 3) == 0) || ((i % 5) == 0))
+		{
+			add = add + i;
+		}
+	}
+	printf("%d\n", add);
+	return (0);
+}
