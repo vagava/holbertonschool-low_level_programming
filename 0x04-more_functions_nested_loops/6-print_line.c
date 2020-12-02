560c58bd3c4060855f412923a1b5f83fc60486f9
vagava <vagava.123@gmail.com>
2020-12-01 23:30:15 -0500

    fix_code_condicion n > 0

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,13 +6,12 @@
 void print_line(int n)
 {
 	int i;
-
-	for (i = 0; i <= n; i++)
+	if (n > 0)
 	{
-		if (n <= 0)
-			_putchar('\n');
-		else
+		for (i = 0; i <= n; i++)
+		{
 			_putchar('_');
+		}
 	}
 	_putchar('\n');
 }
