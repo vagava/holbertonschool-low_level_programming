30a527f44a4634c8d93f10ef6628062bce7e11f1
vagava <vagava.123@gmail.com>
2020-12-02 18:44:06 -0500

    fix_code

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,7 +9,7 @@ char *cap_string(char *a)
 	int i, j, k;
 	char b[13] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	for (i = 0, a[i] != '\0', i++)
+	for (i = 0; a[i] != '\0'; i++)
 	{}
 	for (j = 0 ; j < i ; j++)
 	{
