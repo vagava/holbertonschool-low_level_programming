8ab52722b7f17939f4684d95f07772b1ab4d5127
vagava <vagava.123@gmail.com>
2020-12-02 15:00:46 -0500

    update_header

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,5 +1,8 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
+#include <stdio.h>
+#include <unistd.h>
+
 void reset_to_98(int *n);
 void _puts(char *str);
 void swap_int(int *a, int *b);
@@ -7,5 +10,10 @@ int _strlen(char *s);
 int _putchar(char c);
 void print_rev(char *s);
 void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif
