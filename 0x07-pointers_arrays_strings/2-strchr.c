064041ec0838033815dcdd87f8ee04c15f0c1c6f
vagava <vagava.123@gmail.com>
2020-12-02 20:51:59 -0500

    fix_code4

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -20,17 +20,3 @@ char *_strchr(char *s, char c)
 	}
 	return (0);
 }
-
-int main(void)
-{
-    char *s = "hello";
-    char *f;
-
-    f = _strchr(s, 'l');
-
-    if (f != NULL)
-    {
-        printf("%s\n", f);
-    }
-    return (0);
-}
