7313d4f91e752cccf2eccfe7628a648f944fa230
vagava <vagava.123@gmail.com>
2020-12-02 19:24:51 -0500

    update_header

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -16,5 +16,8 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 int _putchar(char c);
+char *infinite_add(char *n1, char *n2, char *r, int n);
+int _len(char *ptr);
+void print_buffer(char *b, int size);
 
 #endif
