698bbd4d753bb48beb3325734da5c129fa8c2d05
vagava <vagava.123@gmail.com>
2020-12-02 20:28:44 -0500

    add_1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _memcpy - copies memory area.
+ * @dest: memory area dest
+ * @src: memory area src
+ * @n: number of bytes.
+ * Return: pointer to memory area dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = stc[i];
+	}
+	return (dest);
+}
