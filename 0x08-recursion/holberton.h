15f4a79f4ea741c199571f4aa02f1c3d6d8b72d9
vagava <vagava.123@gmail.com>
2020-12-02 23:04:48 -0500

    upgrade_header

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,13 +1,11 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
-void reset_to_98(int *n);
-void _puts(char *str);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-int _putchar(char c);
-void print_rev(char *s);
-void rev_string(char *s);
+#include <stdio.h>
+
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
+int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
