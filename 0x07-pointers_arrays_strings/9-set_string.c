f4b8cbc04ad545242656a573b100258459ea496d
vagava <vagava.123@gmail.com>
2020-12-02 22:52:32 -0500

    add_9

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: double pointer
+ * @to: pointer to assign
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
