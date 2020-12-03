1cc2cb05f6695348226cd242c61b1047bb5f48c8
vagava <vagava.123@gmail.com>
2020-12-03 10:30:46 -0500

    fix_code_remove_main

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -37,18 +37,3 @@ char *_strdup(char *str)
 	ptr[i] = '\0';
 	return (ptr);
 }
-
-int main(void)
-{
-    char *s;
-
-    s = _strdup(("Holberton School!");
-    if (s == NULL)
-    {
-        printf("failed to allocate memory\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
-    return (0);
-}
