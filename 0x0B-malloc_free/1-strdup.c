26035d6878a9b011d731b2f5cc36788229d1253e
vagava <vagava.123@gmail.com>
2020-12-03 10:24:13 -0500

    fix_code_add_len

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,18 @@
 #include "holberton.h"
 /**
+ * _len - serch of size of a  string
+ * @string: stringn.
+ * Return: size of string.
+ */
+int _len(char *string)
+{
+	int len;
+
+	for (len = 0; string[len] != '\0'; len++)
+	;
+	return (len);
+}
+/**
  * _strdup - contains a copy of the string given as a parameter.
  * @str: string to  copy
  * Return: pointer to new string.
