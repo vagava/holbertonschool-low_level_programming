896b43e7055100b82b142c736942ee70bf2a274c
vagava <vagava.123@gmail.com>
2020-12-03 13:27:47 -0500

    fix_code

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,7 +10,7 @@ int **alloc_grid(int width, int height)
 	int i, j;
 	int **ptr;
 
-	if (width < 0 || height < 0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
 	ptr = (int **)malloc(height * (sizeof(int *)));
