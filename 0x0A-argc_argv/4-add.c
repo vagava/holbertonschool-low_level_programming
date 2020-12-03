dc051b219e4dceaec971413a867bdfe718667409
vagava <vagava.123@gmail.com>
2020-12-03 01:25:24 -0500

    add_4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * main - entri point
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: 1 if error, else 0.
+ */
+int main(int argc, char **argv)
+{
+	int i, j, add = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	for (i = 0; i < argc ; i++)
+	{
+		add += atoi(argv[1]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
