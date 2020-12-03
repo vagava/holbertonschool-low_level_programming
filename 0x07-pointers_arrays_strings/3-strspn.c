e5d36605e70150d98fa20dcd0d27dd61c4cc70de
vagava <vagava.123@gmail.com>
2020-12-02 21:09:20 -0500

    fix_code2

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,9 +10,9 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int res = 0;
 	unsigned int i, j;
 
-	for (j = 0; s[j] != ' '; j++)
+	for (i = 0; accept[i] != '\0'; i++)
 	{
-		for (i = 0; accept[i] != '\0'; i++)
+		for (j = 0; s[j] != ' '; j++)
 		{
 			if (accept[i] == s[j])
 				res++;
