96cc50ac46e2c08e99bcaa725549ad7f47c19d47
vagava <vagava.123@gmail.com>
2020-12-03 01:02:31 -0500

    add_3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * main - entri point
+ * @argc: number of arguments
+ * @argv: arguments
+ * return 1 if error, else 0.
+ */
+int main (int argc, char ** argv)
+{
+	int a = 0, b = 0;
+
+	if(argc != 3)
+	{
+		printf("Error\n");
+		return(1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	printf("%d\n", (a * b));
+	return (0);
+}
