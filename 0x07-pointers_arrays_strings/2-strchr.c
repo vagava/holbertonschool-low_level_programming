9c8b0ca7e1610ac5a7e15c8fe1da85931af3a866
vagava <vagava.123@gmail.com>
2020-12-02 20:38:31 -0500

    add_2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _strchr - locates a character in a string.
+ * @s: string
+ * @c: character to find
+ * Return: pointer to character to find or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	char *matched;
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			matched = &s[i];
+			return (matched);
+		}
+	}
+	return (NULL);
+}
