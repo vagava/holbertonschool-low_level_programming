d17c109c525621a63c85a53a1bdf8b5c68bcb2ce
vagava <vagava.123@gmail.com>
2020-12-03 20:48:08 -0500

    add_2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+/**
+ * print_dog- initialize a variable of type struct dog.
+ * @d: a pointer to the structure.
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if ((d->name) == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+
+		printf("Age: %f\n", d->age);
+
+		if ((d->owner) == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+}
