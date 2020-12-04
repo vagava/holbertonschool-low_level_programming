8e3ea052c08c20cf834a820d0f6704dce02b82b2
vagava <vagava.123@gmail.com>
2020-12-03 23:53:49 -0500

    fix_code

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,20 +1,12 @@
-	extern printf
-	global main
-
-	section .text
-main
-	push rbp
-
-	mov rdi, fmt 		;arg3, length of string
-	mov rsi, msg 		;arg2, pointer to string
-	mov rax, 0 		;arg1, write to screen
-	call printf
-
-	pop rbp
-
-	mov rax,0
-	ret
-msg:
-	db "Hello, Holberton", 0
-fmt:
-	db "%s", 10, 0
+#include "lists.h"
+void print_fast(void) __attribute__ ((constructor));
+/**
+* print_fast - prints before main
+*
+* Return: void
+*/
+void print_fast(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
