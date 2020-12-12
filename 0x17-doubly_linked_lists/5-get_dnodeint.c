b5ebdbee2fb9cf025a8298994c6e008c7390eae7
vagava <vagava.123@gmail.com>
2020-12-12 18:53:28 -0500

    add_5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include"lists.h"
+/**
+ * get_dnodeint_at_index - returns the nth node
+ * @head: head
+ * @index: nth node
+ * Return: returns pointer of the nth node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int compare;
+	dlistint_t *temp;
+
+	if (head == NULL)
+		return (NULL);
+	if(index == 0)
+		return(head);
+
+	temp = head;
+	for (compare = 0; compare != index; compare++)
+	{
+		temp = temp->next;
+	}
+
+	return (temp);
+}
