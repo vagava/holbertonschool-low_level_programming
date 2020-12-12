cf312f0ddd7536d0a85542996e763b5994c8633f
vagava <vagava.123@gmail.com>
2020-12-12 16:19:06 -0500

    add_p

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all the elements
+ * @h: head
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	unsigned int counter = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		counter++;
+		h = (*h).next;
+	}
+	return (counter);
+}
