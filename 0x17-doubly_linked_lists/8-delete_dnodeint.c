80aebfac20bd5ea399e56a492f33fc92a1fa3fb4
vagava <vagava.123@gmail.com>
2020-12-13 17:54:18 -0500

    fixed logica

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -7,15 +7,13 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	unsigned int compare;
-	dlistint_t *temp, *temp1;
+	unsigned int compare = 0;
+	dlistint_t *temp;
 
 	if (*head == NULL)
 		return (-1);
 
 	temp = *head;
-	temp1 = temp->next;
-
 	if (index == 0)
 	{
 		if (!temp)
@@ -24,16 +22,14 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		free(temp);
 		return (1);
 	}
-	for (compare = 0; compare < index - 1; compare++)
-	{
-		temp = temp->next;
-		temp1 = temp1->next;
-		if ((compare < index - 1) && (temp->next == NULL))
-			return (-1);
-	}
-		temp->next = temp1->next;
-		temp->next->prev = temp;
-		free(temp1);
-
-	return (1);
+	while (compare < index - 1)
+        {
+                if ((compare < index - 1) && (!temp))
+                        return (-1);
+                temp = temp->next;
+        }
+        temp->next->prev = temp->prev;
+        temp->prev->next = temp->next;
+        free(temp);
+        return (1);
 }
