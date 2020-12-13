2253f292f159cf3cb6761de7128a99d4de6ba048
vagava <vagava.123@gmail.com>
2020-12-13 14:43:09 -0500

    new function added, add condicional head

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+dlistint_t *addd_dnodeint(dlistint_t **head, const int n);
 /**
  * add_dnodeint_end - adds a new node at the end of a linked list.
  * @head: head
@@ -9,8 +10,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node, *temp;
 	/*verificar si hay un nodo existente*/
-	if (!*head)
-		return (add_dnodeint(head, n));
+	if (!*head || !head)
+		return (addd_dnodeint(head, n));
 
 	/*crear nuevo node*/
 	new_node = malloc(sizeof(dlistint_t));
@@ -29,5 +30,31 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	(*new_node).next = NULL;
 	/*enlazar ultimo nodo con el nuevo*/
 	temp->next = new_node;
+	return (new_node);
+}
+
+/**
+ * add_dnodeint - adds a new node at the beginning
+ * @head: head
+ * @n: value of n.
+ * Return:pointer to new node.
+ */
+dlistint_t *addd_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	if (*head)
+		(*head)->prev = new_node;
+
+	(*new_node).n = n;
+	(*new_node).prev = NULL;
+	(*new_node).next = *head;
+
+	*head = new_node;
+
 	return (new_node);
 }
