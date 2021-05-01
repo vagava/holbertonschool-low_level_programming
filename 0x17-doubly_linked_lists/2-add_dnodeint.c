9905e222610656b4030443eded17e6349ba3a076
vagava <vagava.123@gmail.com>
2021-05-01 17:03:11 -0500

    if head == null return null

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (!new)
 		return (NULL);
 	if(!*head)
-		new->next = NULL;
+		return (NULL);
 	else
 		new->next = *head;
 	*head = new;
