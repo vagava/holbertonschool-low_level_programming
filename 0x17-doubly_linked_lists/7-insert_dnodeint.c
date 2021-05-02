68442a66a6423378e907536fb1866e711e89edb1
vagava <vagava.123@gmail.com>
2021-05-01 20:15:55 -0500

    create function is_firstnode and is_othernode

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,40 @@
 #include "lists.h"
 /**
+ * is_firstnode - add new node at the start
+ * @h: head of dlistint_t list.
+ * @new: new node
+ * @n: value of the new node
+ * Return: new node
+ */
+dlistint_t *is_firstnode(dlistint_t **h, dlistint_t *new, int n)
+{
+	dlistint_t *tmp = *h;
+
+	*h = new;
+	new->prev = NULL;
+	new->n = n;
+	new->next = tmp;
+	tmp->prev = new;
+	return (new);
+}
+/**
+ * is_othernode - add new node at the index
+ * @tmp: node reference
+ * @new: new node
+ * @n: value of the new node
+ * Return: new node
+ */
+dlistint_t *is_othernode(dlistint_t *tmp, dlistint_t *new, int n)
+{
+	new->prev = tmp->prev;
+	tmp->prev->next = new;
+	new->n = n;
+	new->next = tmp;
+	tmp->prev = new;
+	return (new);
+}
+
+/**
  * insert_dnodeint_at_index - inserts a new node at a given position.
  * @h: head of dlistint_t list.
  * @idx: index of the list where the new node should be added.
@@ -30,19 +65,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (iterator == idx)
 		{
 			if (idx == 0)/*if list not empty*/
-			{
-				new->prev = NULL;
-				*h = new;
-			}
+				return (is_firstnode(h, new, n));
 			else
-			{
-				new->prev = tmp->prev;
-				tmp->prev->next = new;
-			}
-			new->n = n;
-			new->next = tmp;
-			tmp->prev = new;
-			return (new);
+				return (is_othernode(tmp, new, n));
 		}
 		tmp = tmp->next;
 		iterator++;
