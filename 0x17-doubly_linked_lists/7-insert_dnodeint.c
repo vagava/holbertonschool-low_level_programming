d89102fea7019d5942f60f78667c6cfaa4119607
vagava <vagava.123@gmail.com>
2021-05-01 20:21:48 -0500

    add condicion if index < 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -49,7 +49,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new = NULL;
 	dlistint_t *tmp = NULL;
 
-	if (!h)
+	if (!h || idx < 0)
 		return (NULL);
 	if (!*h && idx == 0) /*if list is empty*/
 	{
