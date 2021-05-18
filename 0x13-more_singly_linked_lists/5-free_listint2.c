f559279469c72911b02e6db440620c5191a103cf
vagava <vagava.123@gmail.com>
2021-05-17 22:22:58 -0500

    added if(head == NULL) condition

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,11 +8,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *aux = NULL;
 
-	while (*head)
+	if (!head)
+		return;
+	while ((*head)->next)
 	{
 		aux = *head;
 		*head = (*head)->next;
 		free(aux);
 	}
 	free(*head);
+	*head = NULL;
 }
