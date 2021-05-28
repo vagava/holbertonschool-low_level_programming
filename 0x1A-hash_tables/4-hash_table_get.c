0ee99f7bacb19710ad31d407e0da03921c636195
vagava <vagava.123@gmail.com>
2021-05-28 17:04:15 -0500

    is there are collisions it goes througth the list and looks for the key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,6 +9,7 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	char *value = NULL;
+	hash_node_t *collision = NULL;
 	unsigned long int index = 0;
 
 	if (!ht || !key || (strcmp(key, "") == 0))
@@ -17,7 +18,13 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	index = key_index((const unsigned char *)key, ht->size);
 	if (!ht->array[index])
 		return (NULL);
-
-	value = ht->array[index]->value;
+	/*look for collisions*/
+	collision = ht->array[index];
+	while(collision)
+	{
+		if (strcmp(collision.key, key) == 0)
+			value = collision->value;
+		collision = collision->next;
+	}
 	return (value);
 }
