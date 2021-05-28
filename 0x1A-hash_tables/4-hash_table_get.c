7445baa66c315437ab00a820e0c3453598337809
vagava <vagava.123@gmail.com>
2021-05-28 16:28:36 -0500

    fix betty

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -3,7 +3,8 @@
  * hash_table_get - function that retrieves a value associated with a key.
  * @ht: is the hash table to look into
  * @key: is the key you are looking for
- * Return: value associated with the element, or NULL if key couldn’t be found.
+ * Return: value associated with the element, or NULL if key couldn’t
+ * be found.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
