46269264bbd7d91b1fea18b210f0ea4fd5a60124
vagava <vagava.123@gmail.com>
2021-05-28 14:40:59 -0500

    add condicion if key is NULL or empty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,6 +15,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!ht)
 		return (0);
 
+	if (!key || (strcmp(key, "") == 0))
+		return (0);
+
 	node = malloc(sizeof(hash_node_t));
 	if (!node)
 		return (0);
