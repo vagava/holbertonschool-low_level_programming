799e0657bdee593c38ec8169e53af2b795fccc11
vagava <vagava.123@gmail.com>
2021-05-28 17:57:49 -0500

    change the structure of function, print all collisions of the table by arrapy position

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,28 +6,30 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *aux = NULL;
+	hash_node_t *collision = NULL;
 	unsigned long int i = 0;
 	int is_empty = 0;
 
-	if(!ht)
+	if (!ht || !ht->array)
 		return;
 	printf("{");
 	while (i < ht->size)
 	{
 		if (ht->array[i] != NULL)
 		{
-			if (is_empty == 0)
-				printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
-			else
-				printf(", '%s': '%s'", ht->array[i]->key, ht->array[i]->value);
-			aux = ht->array[i]->next;
-			while(aux)
+			/*look for collisions*/
+			collision = ht->array[i];
+			while (collision)
 			{
-				printf(", '%s': '%s'", aux->key, aux->value);
-				aux = aux->next;
+				if (is_empty == 0)
+				{
+					printf("'%s': '%s'", collision->key, collision->value);
+					is_empty = 1;
+				}
+				else
+					printf(", '%s': '%s'", collision->key, collision->value);
+				collision = collision->next;
 			}
-			is_empty = 1;
 		}
 		i++;
 	}
