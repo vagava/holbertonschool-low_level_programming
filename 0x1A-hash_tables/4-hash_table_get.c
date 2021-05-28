efe191de844370233d5333ff1906d8c123ed89aa
vagava <vagava.123@gmail.com>
2021-05-28 16:34:11 -0500

    added condition if not key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,6 +13,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	if (!ht)
 		return (NULL);
+	if (!key)
+		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
 	if (!ht->array[index])
